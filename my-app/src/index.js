457ce9257c9efb4f5abda1207272fd988f495daf
Andrej Kaczanowski <andykaczanowski@gmail.com>
2023-01-10 15:14:05 +0100

    import provider

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./reducers/localStorageReducer";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const store = createStore(reducer);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
